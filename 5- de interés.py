cb3413b7874e64d5da61583f823d7c6f884e893d
Dayron-Andrey-Martinez <dayronandreymartinez@gmail.com>
2025-12-02 06:36:30 -0500

    Add files via upload
    Este programa esta enfocado en hacer un 5% de interes

---
+++ 5- de interés.py
@@ -0,0 +1,15 @@
+def capturar_datos_1():
+    dinero = float(input("Digite la cantidad de dinero: "))
+    return dinero
+
+def analizar_datos(dinero):
+    interes = dinero * 0.05
+    return interes
+
+def mostrar_resultados(interes):
+    print("El interés del 5% es:", interes)
+
+
+dinero = capturar_datos_1()
+interes = analizar_datos(dinero)
+mostrar_resultados(interes)
