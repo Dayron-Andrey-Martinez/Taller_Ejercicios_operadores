53dadf254abd0b0168f5651439e230629131eec8
Dayron-Andrey-Martinez <dayronandreymartinez@gmail.com>
2025-12-02 07:08:00 -0500

    Add files via upload
    Calcular el area de rectagulo

---
+++ Area_rectangulo.py
@@ -0,0 +1,23 @@
+# ======== AREA DEL RECTANGULO ========
+
+def capturar_longitud():
+    longitud = float(input("Digite la longitud del rectángulo: "))
+    return longitud
+
+def capturar_ancho():
+    ancho = float(input("Digite el ancho del rectángulo: "))
+    return ancho
+
+def analizar_datos(longitud, ancho):
+    area = longitud * ancho
+    return area
+
+def mostrar_resultados(area):
+    print("El área del rectángulo es:", area)
+
+
+# MAIN
+Longitud= capturar_longitud()
+Ancho = capturar_ancho()
+area = analizar_datos(Longitud, Ancho)
+mostrar_resultados(area)
