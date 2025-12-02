55e613ccd89528452961e18f999dbc5eed6ccdab
Dayron-Andrey-Martinez <dayronandreymartinez@gmail.com>
2025-12-02 06:49:13 -0500

    Add files via upload
    Esta enfocado en  hayar el area de un trapecio, pidiendo sus bases y alturas especificas

---
+++ Area de de un trapecio.py
@@ -0,0 +1,28 @@
+# ======== AREA DEL TRAPECIO ========
+
+def capturar_base_mayor():
+    Base_mayor = float(input("Digite la base mayor del trapecio: "))
+    return Base_mayor
+
+def capturar_base_menor():
+    base_menor = float(input("Digite la base menor del trapecio: "))
+    return base_menor
+
+def capturar_altura():
+    altura = float(input("Digite la altura del trapecio: "))
+    return altura
+
+def analizar_datos(Base_mayor, basa_menor, altura):
+    area = ((Base_mayor + basa_menor) * altura) / 2
+    return area
+
+def mostrar_resultados(area):
+    print("El área del trapecio es:", area)
+
+
+# MAIN
+Base_mayor = capturar_base_mayor()
+base_menor = capturar_base_menor()
+altura = capturar_altura()
+area = analizar_datos(Base_mayor, base_menor, altura)
+mostrar_resultados(area)
